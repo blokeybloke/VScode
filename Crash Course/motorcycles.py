023d2e3bac5e6c0af1eff8f7dbeff6a546cd6a35
blokeybloke <152946274+blokeybloke@users.noreply.github.com>
2023-12-26 15:04:52 +1100

    Update

--- Crash Course/motorcycles.py
+++ Crash Course/motorcycles.py
@@ -64,4 +64,43 @@ print(motorcycles)
 print(f"\nA {too_gay.title()} is too gay for me.")
 
 # Challenge - Guest list
-guests = [""]
+guests = ["Candace Owens", "Joe Rogan", "Charlie Munger"]
+print(f"Hi {guests[0]}, would you like to come to dinner?")
+print(f"Hi {guests[1]}, would you like to come to dinner?")
+print(f"Hi {guests[2]}, would you like to come to dinner?")
+# Changing guest list
+cannot_attend = guests.pop()
+print(f"{cannot_attend}, can no longer attend dinner.")
+
+guests.append("Elon Musk")
+print(f"Hi {guests[0]}, would you like to come to dinner?")
+print(f"Hi {guests[1]}, would you like to come to dinner?")
+print(f"Hi {guests[2]}, would you like to come to dinner?")
+# More guests
+print("I have found a bigger table and will be inviting 3 more guests.")
+guests.insert(0, "Mike Tyson")
+guests.insert(2, "Michael Jackson")
+guests.append ("Donald Trump")
+print(guests)
+#Shrinking guest list
+new_guests = ['Mike Tyson', 'Candace Owens', 'Michael Jackson', 'Joe Rogan', 'Elon Musk', 'Donald Trump']
+print(new_guests)
+kicked = new_guests.pop()
+print(f"Sorry {kicked} you have been kicked out.")
+print(new_guests)
+kicked = new_guests.pop()
+print(f"Sorry {kicked} you have been kicked out.")
+print(new_guests)
+kicked = new_guests.pop()
+print(f"Sorry {kicked} you have been kicked out.")
+print(new_guests)
+kicked = new_guests.pop()
+print(f"Sorry {kicked} you have been kicked out.")
+print(new_guests)
+
+print(f"Hi {new_guests[0]}, you are still invited.")
+print(f"Hi {new_guests[1]}, you are still invited.")
+
+del new_guests[0]
+del new_guests[0]
+print(new_guests)
