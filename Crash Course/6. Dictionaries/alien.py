5a1082940f38c55c9a3ee8d5df317483db2c2981
blokeybloke <152946274+blokeybloke@users.noreply.github.com>
2024-01-11 16:31:04 +1100

    Update

--- Crash Course/6. Dictionaries/alien.py
+++ Crash Course/6. Dictionaries/alien.py
@@ -30,3 +30,36 @@ print(f"Alien was {alien_0['colour']}.")
 
 alien_0["colour"] = "yellow"
 print(f"Now the alien is {alien_0['colour']}")
+
+alien_0 = {"x_postion": 0, "y_position": 25, "speed": "medium"}
+print(f"Original position: {alien_0['x_postion']}")
+
+# Move alien to the right
+# Determine how far to move the alien based on its current speed
+
+if alien_0["speed"] == "slow":
+    x_increment = 1
+elif alien_0["speed"] == "medium":
+    x_increment = 2
+else:
+    x_increment = 3
+
+# New position = old position + increment
+alien_0["x_postion"] = alien_0["x_postion"] + x_increment
+
+print(f"New position: {alien_0['x_postion']}")
+
+# Turn into fast alien
+alien_0["speed"] = "fast"
+
+# Removing key points
+alien_0 = {"colour": "green", "points": 5}
+print(alien_0)
+
+del alien_0["points"]
+print(alien_0)
+
+# get() to access values
+alien_0 = {"colour": "green", "speed": "slow"}
+point_value = alien_0.get("points", "No point value assigned.")
+print(point_value)
---
+++ Crash Course/6. Dictionaries/favourite_languages.py
@@ -0,0 +1,18 @@
+favourite_languages = {
+    "jen": "python",
+    "sarah": "c",
+    "edward": "rust",
+    "phil": "python"
+    }
+
+language = favourite_languages["sarah"].title()
+print(f"Sarah's favourite language is {language}.")
+
+# Challenge - Person
+vien = {"first": "vien", "last": "diep", "age": 35, "city": "cabramatta"}
+print(vien["first"])
+print(vien["last"])
+print(vien["age"])
+print(vien["city"])
+
+# Favourite numbers
