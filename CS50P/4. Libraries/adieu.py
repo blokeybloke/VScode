830d35103e73e7e4cf4acf5445372d141bf346e3
blokeybloke <152946274+blokeybloke@users.noreply.github.com>
2024-01-29 21:02:38 +1100

    Update

--- CS50P/4. Libraries/adieu.py
+++ CS50P/4. Libraries/adieu.py
@@ -1,8 +1,22 @@
-people = ["liesl", "friedrich", "louisa", "kurt", "brigitta", "marta", "gretl"]
+people = ["Liesl", "Friedrich", "Louisa", "Kurt", "Brigitta", "Marta", "Gretl"]
 
-user = input("Name: ")
+valid_names = []
 
-for peep in people:
-    if peep in people:
-        print(f"Adieu, adieu, to {people[0].title()}")
+while True:
+    user = input("Name: ")
+    
+    if user.lower() == "exit":
         break
+
+    name = user.strip(). capitalize()
+
+    if name in people:
+        valid_names.append(name)
+
+
+if valid_names:
+    if len(valid_names) > 1:
+        rule = ", ".join(valid_names[:-1]) + " and " + valid_names[-1]
+    else:
+        rule = valid_names[0]
+    print(f"Adieu, adieu, to {rule}")
