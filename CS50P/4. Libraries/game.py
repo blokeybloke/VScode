fde05d29ceaad4b6ca6b1c8b439d1b429d85e6d3
blokeybloke <152946274+blokeybloke@users.noreply.github.com>
2024-02-12 21:01:35 +1100

    Update

--- CS50P/4. Libraries/game.py
+++ CS50P/4. Libraries/game.py
@@ -1,7 +1,18 @@
 import random
 
-user = int(input("Level: "))
-n = user
-number = random.randint(1, n)
+while True:
+    try:
+        user = int(input("Level: "))
+        n = user
+        number = random.randint(1, n)
+        
+        
+    except ValueError:
+        print(user)
 
-print(user)
+
+
+#n = user
+#number = random.randint(1, n)
+
+#print(user)
