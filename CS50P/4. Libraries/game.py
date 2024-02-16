9cecb4937682a8f0404c133d9df9cc928a334e8c
blokeybloke <152946274+blokeybloke@users.noreply.github.com>
2024-02-16 14:10:57 +1100

    Update

--- CS50P/4. Libraries/game.py
+++ CS50P/4. Libraries/game.py
@@ -3,16 +3,27 @@ import random
 while True:
     try:
         user = int(input("Level: "))
-        n = user
-        number = random.randint(1, n)
-        
-        
+        if user >0:
+            break
     except ValueError:
-        print(user)
+        pass
+
+random_numb = random.randint(1, user)
+
+while True:
+    try:
+        guess = int(input("Guess: "))
+        if guess <= 0:
+            print("Too small!")
+        elif guess < random_numb:
+            print("Too small!")
+        elif guess > random_numb:
+            print("Too large!")
+        else:
+            print("Just right!")
+            break
+    except ValueError:
+        pass
 
 
 
-#n = user
-#number = random.randint(1, n)
-
-#print(user)
